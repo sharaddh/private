6c5b6364f5b3e67347be603edc169290414bf994
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-11 23:16:50 +0530

    privte

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Customers from "./pages/Customers";
 import Orders from "./pages/Orders";
 import Bills from "./pages/Bills";
@@ -8,49 +9,22 @@ import InventoryPage from "./pages/InventoryPage";
 import Delivery from "./pages/Delivery";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <div className="min-h-screen p-6">
-      <header className="mb-6">
-        <h1 className="text-2xl font-bold">KMJ Optical ERP</h1>
-        <nav className="mt-2">
-          <Link to="/customers" className="mr-4">
-            Customers
-          </Link>
-          <Link to="/orders" className="mr-4">
-            Orders
-          </Link>
-          <Link to="/bills" className="mr-4">
-            Bills
-          </Link>
-          <Link to="/payments" className="mr-4">
-            Payments
-          </Link>
-          <Link to="/inventory" className="mr-4">
-            Inventory
-          </Link>
-          <Link to="/delivery" className="mr-4">
-            Delivery
-          </Link>
-          <Link to="/login" className="mr-4">Login</Link>
-          <Link to="/register" className="mr-4">Register</Link>
-        </nav>
-      </header>
-
-      <main>
-        <Routes>
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/bills" element={<Bills />} />
-          <Route path="/payments" element={<Payments />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/delivery" element={<Delivery />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route index element={<div>Welcome to KMJ ERP</div>} />
-        </Routes>
-      </main>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/bills" element={<Bills />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/delivery" element={<Delivery />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Layout>
   );
 }
