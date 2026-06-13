ed34782ab05e7ece23b9da7c601657d3d588039c
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-13 23:43:09 +0530

    private

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,7 +25,6 @@ app.use("/api", routes);
 
 app.get("/", (req, res) => res.json({ success: true, message: "KMJ ERP API" }));
 
-// Global error handler
 app.use((err: any, req: any, res: any, next: any) => {
   console.error(err);
   res.status(500).json({ success: false, message: "Internal Server Error" });
