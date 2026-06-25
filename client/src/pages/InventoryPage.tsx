d56b46c54e303a3ce150c0a5a7e40d3ce9a7b293
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-26 00:11:10 +0530

    chore: remove unused useCallback import from InventoryPage

--- client/src/pages/InventoryPage.tsx
+++ client/src/pages/InventoryPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import api from "../api";
 import { useCachedData } from "../hooks/useCachedData";
 import Table from "../components/Table";
