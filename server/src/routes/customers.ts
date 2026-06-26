e0b4f881eee7cb57793fce3de45661e1e0b27be1
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-06-27 00:11:06 +0530

    feat: cache customers GET routes and invalidate on writes

--- server/src/routes/customers.ts
+++ server/src/routes/customers.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth";
+import { cacheRoute, invalidateCache } from "../middleware/cache";
 import * as customerController from "../controllers/customerController";
 
 const router = Router();
 
-router.get("/", authenticate, customerController.getAll);
-router.post("/", authenticate, customerController.create);
-router.get("/summary/:id", authenticate, customerController.getSummary);
+router.get("/", authenticate, cacheRoute(60), customerController.getAll);
+router.post("/", authenticate, (req, res, next) => {
+  invalidateCache("/api/customers");
+  customerController.create(req, res, next);
+});
+router.get("/summary/:id", authenticate, cacheRoute(30), customerController.getSummary);
 router.get("/:id", authenticate, customerController.getById);
-router.put("/:id", authenticate, customerController.update);
-router.delete("/:id", authenticate, customerController.remove);
+router.put("/:id", authenticate, (req, res, next) => {
+  invalidateCache("/api/customers");
+  customerController.update(req, res, next);
+});
+router.delete("/:id", authenticate, (req, res, next) => {
+  invalidateCache("/api/customers");
+  customerController.remove(req, res, next);
+});
 
 export default router;
