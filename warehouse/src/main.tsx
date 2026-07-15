977c7805cbbeecc442778e57c11098647826460f
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-07-15 18:39:37 +0530

    feat: add ThemeProvider, ToastProvider, and ErrorBoundary to main entry

--- warehouse/src/main.tsx
+++ warehouse/src/main.tsx
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { ToastProvider } from "./context/ToastContext";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
+    <ErrorBoundary>
+      <HashRouter>
+        <ThemeProvider>
+          <AuthProvider>
+            <ToastProvider>
+              <App />
+            </ToastProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </HashRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
