550d3da53ed152ef65c581251c7e593e8f81ed4c
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-07-16 22:52:20 +0530

    fix: add scrollbar-none CSS class and lenis smooth scroll for entire app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import Lenis from "lenis";
 import AppProviders from "./context/AppProviders";
 import App from "./App";
 import "./index.css";
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  touchMultiplier: 2,
+});
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
