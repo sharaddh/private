8ba2610009202a62a366ddd131496421932322fb
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-07-19 23:23:05 +0530

    fix: wire sendBillWhatsApp after workspace transaction

--- server/src/controllers/workspaceController.ts
+++ server/src/controllers/workspaceController.ts
@@ -1,9 +1,14 @@
 import { BranchRequest } from "../types";
 import { Response } from "express";
-import { executeTransaction } from "../services/workspace.service";
+import { executeTransaction, sendBillWhatsApp } from "../services/workspace.service";
 import { sendSuccess } from "../utils/response";
 
 export async function transaction(req: BranchRequest, res: Response) {
   const data = await executeTransaction(req.body, req.branchId);
+  const bill = data.bill as any;
+  const customer = data.customer as any;
+  if (bill && customer) {
+    sendBillWhatsApp(bill, customer, req.branchId);
+  }
   sendSuccess(res, data, "Transaction completed");
 }
